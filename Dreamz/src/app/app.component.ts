2c14b46723fbb70d8a12e89f9b00930232c19da0
MMMalvin <malvinmaina23@gmail.com>
2023-10-25 13:17:16 +0200

    Added Login, Register Pages

--- Dreamz/src/app/app.component.ts
+++ Dreamz/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dreamz';
+export class AppComponent implements OnInit {
+  tabRegister: Boolean = false;
+  tabLogin: Boolean = true;
+
+
+  ngOnInit(): void {
+    
+  }
+
+  onTabRegisterClick(){
+    this.tabLogin = false;
+    this.tabRegister = true;
+    console.log("Register click")
+  }
+
+  onTabLoginClick(){
+    this.tabLogin = true;
+    this.tabRegister = false;
+    console.log("Login click")
+  }
 }
